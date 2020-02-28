0db270c4010e5a7e7921c6db5537c2b513c228b1
Anthony Tristan-Padilla <aatris39>
2020-02-28 11:17:57 -0500

    1

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ h = 100
 w = 100
 score = 0
 
-
+#images
 win = pygame.display.set_mode((800, 600))
 img = pygame.image.load('assets/characters/MC.png')
 img = pygame.transform.scale(img, (100,100))
@@ -18,14 +18,16 @@ img2 = pygame.image.load('assets/characters/CH2.png')
 img3 = pygame.image.load('assets/characters/CH3.png')
 img4 = pygame.image.load('assets/characters/CH4.png')
 img5 = pygame.image.load('assets/milk.png')
+img6 = pygame.image.load('assets/text.png')
+img6 = pygame.transform.scale(img6, (50,70))
 
-
-font = pygame.font.SysFont("arial", 40)
-text = font.render("Pour the milk first b**ch", True, (0, 0, 0))
-font = pygame.font.SysFont("arial", 20)
-text2 = font.render("A story by: Anthony :)", True, (0, 0, 0))
+#text
 font = pygame.font.SysFont("arial", 15)
-text3 = font.render("SCORE: " + score)
+text = font.render("Catch the milk b**ch", True, (0, 0, 0))
+font = pygame.font.SysFont("arial", 10)
+text2 = font.render("collect the milk for points!", True, (0, 0, 0))
+font = pygame.font.SysFont("arial", 15)
+text3 = font.render("SCORE: " + str(score), True, (0,0,0))
 
 run = True
 while run:
@@ -34,15 +36,19 @@ while run:
             run = False
     win.fill((255, 255, 255))
 
+#drawing images and text
     win.blit(img, (x1, y1))
     #win.blit(img2, (190, 0))
     #win.blit(img3, (190, 0))
     #win.blit(img4, (190, 0))
     win.blit(img5, (mx, my))
+    #win.blit(img6, (20,120))
 
-    #win.blit(text, (190, 0))
-    #win.blit(text2, (290, 40))
-    win.blit(text3, (290, 40))
+    win.blit(text, (665, 0))
+    win.blit(text2, (680, 20))
+    
+    #if score > 300:
+        #win.blit(img2, (0, 120))
 
     if my >= -48:
        my += .3
@@ -51,13 +57,16 @@ while run:
         my = -48
         mx = random.randint(0, 780)
 
+#hitbox for score
     if x1 < mx and y1 < my and y1 < my + h and x1 < mx + w:
         score += 1
         print(score)
     
+    text3 = font.render("SCORE: " + str(score), True, (0, 0, 0))
+    win.blit(text3, (0, 10))
+    
 
-    
-    
+#move character    
     keys=pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
         x1 -= 0.3
